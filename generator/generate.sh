e99c872521a537c3ddc27be69698d776696549a9
itturnsout <jovan.milosevic@gmail.com>
2015-05-04 09:23:39 +0700

    improved generate.sh echoing

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,26 +1,27 @@
-echo "deleting public_html"
+printf "\n"
+printf "deleting public_html\n\n"
 rm -r ../public_html/*
 
-echo "copying posts to public_html"
+printf "copying posts to public_html:\n"
 cd ../private_src/posts
-
 for dir in */
 do
 	if [ -e "$dir"index.html ]
 	then
-		echo "$dir"
+		printf "\t$dir\n"
 		mkdir ../../public_html/"$dir"
 		cp "$dir"index.html ../../public_html/"$dir"
 	fi
 done
+printf "\n"
 
-echo "copying favicon"
+printf "copying favicon\n\n"
 cd ..
 cp favicon.ico ../public_html
 
-echo "generating css"
-echo $PWD
+printf "generating css\n"
 cd ../generator
 compass compile
+printf "\n"
 
-echo "all done"
+printf "all done\n"
